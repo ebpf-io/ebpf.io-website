2f6fbcd491f65363dae08814ef6386f0bb9dd50e
Nick Travers <n.e.travers@gmail.com>
2020-08-16 10:21:03 -0700

    Various spelling and grammar fixups
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,8 @@ Building on the foundation of seeing and understanding all system calls and
 combining that with a packet and socket-level view of all networking operations
 allows for revolutionary new approaches to securing systems. While aspects of
 system call filtering, network-level filtering, and process context tracing
-have typically been handled by completely independent systems, eBPF allows to
-combine the visibility and control of all aspects to create security systems
+have typically been handled by completely independent systems, eBPF allows for
+combining the visibility and control of all aspects to create security systems
 operating on more context with better level of control.
 `;
 
