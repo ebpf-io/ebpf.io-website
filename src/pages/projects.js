c385053b4b31617df7e303161918cb3cf6ce2cd8
Itay Shakury <itay@itaysk.com>
2020-09-17 15:04:26 +0300

    fix tracee image link

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -519,7 +519,7 @@ const ProjectDescriptions = () => (
           target="_blank"
           href="https://github.com/aquasecurity/tracee"
         >
-          <img src="https://github.com/aquasecurity/tracee/blob/master/images/tracee.png" />
+          <img src="https://raw.githubusercontent.com/aquasecurity/tracee/master/images/tracee.png" />
         </a>
         <div className="project-description">
           <p>
