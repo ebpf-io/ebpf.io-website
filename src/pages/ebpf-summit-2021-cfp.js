4b11b599ac0cd4b088a740c0e74e2173d3875f26
Thomas Graf <thomas@cilium.io>
2021-05-24 22:51:42 +0200

    eBPF Summit 2021
    Signed-off-by: Thomas Graf <thomas@cilium.io>

---
+++ src/pages/ebpf-summit-2021-cfp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Redirect } from '@reach/router'
+
+const SummitPage = () => <>
+  <div style={{display: 'grid', placeItems: 'center', height: '100vh', fontSize: '1.5rem'}}>
+    <div style={{textAlign: 'center'}}>
+      Redirecting to <a href="https://ebpf.io/summit-2020" style={{color: '#4385F5'}}>https://ebpf.io/summit-2021</a>…
+    </div>
+  </div>
+
+  <Redirect noThrow to={`/summit-2021`} />
+</>
+
+export default SummitPage
