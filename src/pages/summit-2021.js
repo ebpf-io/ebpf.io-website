5ff731760574c2a2815c379ac1bede5caa4876fd
Liz Rice <liz@lizrice.com>
2021-06-07 19:23:06 +0100

    Use updated Google form
    Signed-off-by: Liz Rice <liz@lizrice.com>

--- src/pages/summit-2021.js
+++ src/pages/summit-2021.js
@@ -9,7 +9,7 @@ import queryString from 'query-string';
 import "../stylesheets/index.scss";
 import "../../node_modules/react-modal-video/scss/modal-video.scss";
 const pageMetaTitle = 'eBPF Summit 2021'
-const pageMetaDescription = 'Registration is now open for the eBPF Summit 2021, a virtual event, targeted at DevOps, SecOps, platform architects, and developers. To be held August 18-19, 2021.'
+const pageMetaDescription = 'Pre-registration is now open for the eBPF Summit 2021, a virtual event, targeted at DevOps, SecOps, platform architects, and developers. To be held August 18-19, 2021.'
 
 const Info = () => (
   <div className="summit-info">
@@ -40,7 +40,7 @@ const Hero = () => (
       </div>
 
       <p className="description">
-        eBPF Summit, a virtual event, targeted at DevOps, SecOps, platform architects, and developers is open for pre-registration. We will open the Call-For-Papers (CFP) soon.<br/>
+        eBPF Summit, a virtual event, targeted at DevOps, SecOps, platform architects, and developers is open for pre-registration. We will open the Call For Papers (CFP) soon.<br/>
       </p>
 
       <div className="space" />
@@ -53,7 +53,7 @@ const Registration = () => (
   <div className="summit-about">
     <div className="wrapper" style={{textAlign: 'center'}}>
       <a name="register" />
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeVuoYbf2cSlczAxGT9S9Cm7w0Yb8W9ALAiKADf5HLe7MjXKQ/viewform?embedded=true" width="640" height="543" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeCoqMgOKVBwbDkQQWmfoYq34ElS16T_hc5mHAP8vO8lQP--A/viewform?embedded=true" width="640" height="583" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   </div>
 );
