40ad5776f05353d566a49125ae8ee23707004d14
Thi Van Le <vannnyle@gmail.com>
2022-10-19 13:11:22 +0200

    fix: blog category slugs

--- src/utils/blog-filter-to-slug.js
+++ src/utils/blog-filter-to-slug.js
@@ -1,6 +1,6 @@
 import slugify from "slugify";
 
 export default function blogFilterToSlug(item) {
-  const category = slugify(item, {remove: /[^a-z0-9\-]/g, lower: true});
+  const category = slugify(item, {lower: true});
     return item === '*' ? '/blog/' : `/blog/categories/${category}/`;
   }
