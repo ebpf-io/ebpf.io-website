398f3e8d767b82cb5688150324f00da868221f1f
Den Bratchikov <75138180+Den-Bratchikov@users.noreply.github.com>
2023-01-12 01:51:15 -0800

    chore: add fallback fonts using fonpie (#322)
    * chore: add fallback fonts using fonpie
    
    * fix: add small fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,6 +7,7 @@ import Dropdown from "../common/header/Dropdown.js";
 import LanguageDropdown from "../common/footer/LanguageDropdown.js";
 import "./footer.scss";
 import "./header.scss";
+import "./fonts.css";
 import "./index.css";
 import "./menu-icon.scss";
 
