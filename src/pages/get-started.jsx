00fb67dcc00e98cfdc66326cd929545673069afc
nerucheva <tatiana@nerucheva.com>
2023-02-15 13:33:18 +0100

    feat(pages): add SubscriptionForm

--- src/pages/get-started.jsx
+++ src/pages/get-started.jsx
@@ -5,11 +5,15 @@ import Documentation from 'components/pages/get-started/documentation/documentat
 import Hero from 'components/pages/get-started/hero';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
+import SubscriptionForm from 'components/shared/subscription-form';
 
 const HomePage = () => (
   <Layout>
     <Hero />
     <Documentation />
+    <div className="container mt-28">
+      <SubscriptionForm size="md" />
+    </div>
   </Layout>
 );
 
