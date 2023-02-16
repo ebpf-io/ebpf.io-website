73f6535f62b1ba03077748e0c680be5e577578d6
nerucheva <tatiana@nerucheva.com>
2023-02-16 11:59:54 +0100

    feat(components): move a component with video gallery to shared components

--- src/pages/get-started.jsx
+++ src/pages/get-started.jsx
@@ -6,6 +6,34 @@ import Hero from 'components/pages/get-started/hero';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 import SubscriptionForm from 'components/shared/subscription-form';
+import VideoGallery from 'components/shared/video-gallery';
+
+const videoItems = [
+  {
+    videoId: 'wnlsHeiTLtE',
+    title: 'How the Hive Came to Bee - A Technical Deep Dive of eBPF',
+    speaker: 'Daniel Borkmann, Cilium',
+    date: 'Dec 8, 2022',
+  },
+  {
+    videoId: '99jUcLt3rSk',
+    title: 'eBPF and Kubernetes: Little Helper Minions for Scaling Microservices',
+    speaker: 'Daniel Borkmann, Cilium',
+    date: 'Sep 4, 2020',
+  },
+  {
+    videoId: 'cABk-6Sdb20',
+    title: 'eBPF on the Rise - Getting Started',
+    speaker: 'Quentin Monnet, Isovalent',
+    date: 'May 14, 2021',
+  },
+  {
+    videoId: 'U3PdyHlrG1o',
+    title: 'BPF as a revolutionary technology for the container landscape',
+    speaker: 'Daniel Borkmann, Cilium',
+    date: 'Aug 7, 2020',
+  },
+];
 
 const HomePage = () => (
   <Layout>
@@ -14,6 +42,7 @@ const HomePage = () => (
     <div className="container mt-28">
       <SubscriptionForm size="md" />
     </div>
+    <VideoGallery items={videoItems} title="Watch videos on how to build infrastructure" />
   </Layout>
 );
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,58 @@
 import React from 'react';
 
 import CaseStudies from 'components/pages/home/case-studies';
-import Community from 'components/pages/home/community';
 import Features from 'components/pages/home/features';
 import Hero from 'components/pages/home/hero';
 import Testimonials from 'components/pages/home/testimonials';
 import WhyEbpf from 'components/pages/home/why-ebpf';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
+import VideoGallery from 'components/shared/video-gallery';
+
+const videoItems = [
+  {
+    videoId: '6N30Yp5f9c4',
+    title: 'BPF and Spectre: Mitigating transient execution attacks',
+    speaker: 'Daniel Borkmann, Isovalent',
+    date: 'Aug 20, 2021',
+  },
+  {
+    videoId: '_5Z2AU7QTH4',
+    title: 'BPF Internals',
+    speaker: 'Brendan Gregg, Netflix',
+    date: 'Jun 9, 2021',
+  },
+  {
+    videoId: 'PJY-rN1EsVw',
+    title: 'Advanced BPF kernel features for the container age',
+    speaker: 'Daniel Borkmann, Isovalent',
+    date: 'Feb 9, 2021',
+  },
+  {
+    videoId: 'slBAYUDABDA',
+    title: 'The Future of eBPF based Networking and Security',
+    speaker: 'Thomas Graf, Isovalent',
+    date: 'Nov 8, 2020',
+  },
+  {
+    videoId: 'Qhm1Zn_BNi4',
+    title: 'BPF as a Fundamentally Better Dataplane',
+    speaker: 'Daniel Borkmann, Isovalent',
+    date: 'Nov 8, 2020',
+  },
+  {
+    videoId: 'ZYBXZFKPS28',
+    title: 'BPF at Facebook',
+    speaker: 'Alexei Starovoitov, Facebook',
+    date: 'Dec 19, 2019',
+  },
+  {
+    videoId: '_Iq1xxNZOAo',
+    title: 'How to Make Linux Microservice-Aware with Cilium and eBPF',
+    speaker: 'Thomas Graf, Isovalent',
+    date: 'Mar 6, 2019',
+  },
+];
 
 const HomePage = () => (
   <Layout>
@@ -17,7 +62,7 @@ const HomePage = () => (
     <WhyEbpf />
     <Testimonials />
     <Features />
-    <Community />
+    <VideoGallery title="eBPF Community Talks" items={videoItems} />
   </Layout>
 );
 
