89754dbf49d7aa9a499581167d0e9e111dc0907f
nerucheva <tatiana@nerucheva.com>
2023-02-20 12:21:51 +0100

    feat: refactoring, updating the data

--- src/components/pages/get-started/books/books.jsx
+++ src/components/pages/get-started/books/books.jsx
@@ -1,32 +1,64 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 
 import Button from 'components/shared/button/button';
 import Link from 'components/shared/link/link';
 
-import bpfPerformanceTools from './images/bpf-performance-tools.jpg';
-import linuxObservabilityWithBpf from './images/linux-observability-with-bpf.jpg';
-import systemsPerformance from './images/systems-performance.jpg';
-import whatIsEbpf from './images/what-is-ebpf.jpg';
-
 const items = [
   {
-    image: whatIsEbpf,
-    alt: "What is eBPF? Liz Rice, O'Reilly, 2022",
+    image: (
+      <StaticImage
+        className="w-full rounded"
+        imgClassName="w-full"
+        src="./images/what-is-ebpf.jpg"
+        alt="What is eBPF? Liz Rice, O'Reilly, 2022"
+        width={356}
+        height={464}
+        loading="lazy"
+      />
+    ),
     linkUrl: 'https://isovalent.com/data/liz-rice-what-is-ebpf.pdf',
   },
   {
-    image: systemsPerformance,
-    alt: 'Systems Performance: Enterprise and the Cloud, 2nd Edition, Brendan Gregg, Addison-Wesley Professional Computing Series, 2020',
+    image: (
+      <StaticImage
+        className="w-full rounded"
+        imgClassName="w-full"
+        src="./images/systems-performance.jpg"
+        alt="Systems Performance: Enterprise and the Cloud, 2nd Edition, Brendan Gregg, Addison-Wesley Professional Computing Series, 2020"
+        width={356}
+        height={464}
+        loading="lazy"
+      />
+    ),
     linkUrl: 'http://www.brendangregg.com/systems-performance-2nd-edition-book.html',
   },
   {
-    image: bpfPerformanceTools,
-    alt: 'BPF Performance Tools, Brendan Gregg, Addison-Wesley Professional Computing Series, Dec 2019',
+    image: (
+      <StaticImage
+        className="w-full rounded"
+        imgClassName="w-full"
+        src="./images/bpf-performance-tools.jpg"
+        alt="BPF Performance Tools, Brendan Gregg, Addison-Wesley Professional Computing Series, Dec 2019"
+        width={356}
+        height={464}
+        loading="lazy"
+      />
+    ),
     linkUrl: 'http://www.brendangregg.com/bpf-performance-tools-book.html',
   },
   {
-    image: linuxObservabilityWithBpf,
-    alt: "Linux Observability with BPF, David Calavera, Lorenzo Fontana, O'Reilly, Nov 2019",
+    image: (
+      <StaticImage
+        className="w-full rounded"
+        imgClassName="w-full"
+        src="./images/linux-observability-with-bpf.jpg"
+        alt="Linux Observability with BPF, David Calavera, Lorenzo Fontana, O'Reilly, Nov 2019"
+        width={356}
+        height={464}
+        loading="lazy"
+      />
+    ),
     linkUrl: 'https://www.oreilly.com/library/view/linux-observability-with/9781492050193/',
   },
 ];
@@ -39,24 +71,17 @@ const Books = () => (
       </h2>
 
       <ul className="mt-16 grid w-full  grid-cols-4 items-center justify-center gap-14 lg:mt-14 lg:gap-8 md:mt-11 md:flex-wrap sm:mt-8 sm:grid-cols-2 xs:grid-cols-1">
-        {items.map(({ image, alt, linkUrl }, index) => (
+        {items.map(({ image, linkUrl }, index) => (
           <li
             className="group relative m-auto drop-shadow-book xs:flex xs:max-w-[256px] xs:justify-center"
             key={index}
           >
             <Link className="" to={linkUrl} target="_blank" rel="noreferrer noopener">
-              <img
-                className="w-full rounded"
-                src={image}
-                alt={alt}
-                width={256}
-                height={334}
-                loading="lazy"
-              />
+              {image}
 
               <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center rounded bg-transparent transition-colors duration-200 group-hover:bg-[#000000]/[0.3] ">
                 <Button
-                  className="py-[16px] px-[28px] opacity-0 transition-opacity duration-200 group-hover:opacity-100"
+                  className="py-4 px-7 opacity-0 transition-opacity duration-200 group-hover:opacity-100"
                   theme="primary-yellow-filled"
                 >
                   Learn More
