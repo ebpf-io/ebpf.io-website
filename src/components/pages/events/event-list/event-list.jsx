399dec9c917f426efe93f17f73739edb326da673
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-03-09 15:43:10 +0300

    feat: adding EventList component

---
+++ src/components/pages/events/event-list/event-list.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+
+import Card from 'components/pages/events/card';
+import SubscriptionForm from 'components/shared/subscription-form';
+
+const EventList = ({ items }) => (
+  <section className="safe-paddings pt-12 pb-16 md:pt-10 md:pb-12">
+    <div className="container grid-gap grid grid-cols-12 sm:flex sm:flex-col sm:gap-y-5">
+      {items.map((item, index) => (
+        <Fragment key={index}>
+          <Card {...item} className="col-span-4 md:col-span-6" />
+          {/* The section that is being displayed on the desktop */}
+          {index === 8 && (
+            <div className="col-span-12 my-[72px] md:!hidden">
+              <SubscriptionForm size="lg" />
+            </div>
+          )}
+          {/* The same section that is being displayed on the mobile */}
+          {index === 5 && (
+            <div className="col-span-12 my-20 hidden md:flex sm:my-12">
+              <SubscriptionForm size="lg" />
+            </div>
+          )}
+        </Fragment>
+      ))}
+    </div>
+  </section>
+);
+
+EventList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default EventList;
