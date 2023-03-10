0d213bf6254d6afa43ad08ee5f4d5f0e4b937a0f
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-03-10 20:25:15 +0300

    feat: gatsby-mdx base configuration

--- gatsby/constants.js
+++ gatsby/constants.js
@@ -3,9 +3,22 @@ const POSTS_REGEX = '/content/blog-posts/((?!README).)*$/';
 const POST_REQUIRED_FIELDS = ['title', 'description', 'date', 'categories'];
 const STATIC_PAGE_REQUIRED_FIELDS = ['title'];
 
+const EVENTS_REGEX = '/content/events/((?!README).)*$/';
+const EVENT_REQUIRED_FIELDS = [
+  'title',
+  'description',
+  'date',
+  'type',
+  'region',
+  'place',
+  'linkUrl',
+];
+
 module.exports = {
   DRAFT_FILTER,
   POSTS_REGEX,
   POST_REQUIRED_FIELDS,
   STATIC_PAGE_REQUIRED_FIELDS,
+  EVENTS_REGEX,
+  EVENT_REQUIRED_FIELDS,
 };
