547081e302b1b0654184c884e1d2ab4e75df7641
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-03-10 20:25:15 +0300

    feat: gatsby-mdx base configuration

---
+++ src/constants/event.js
@@ -0,0 +1,8 @@
+const EVENTS_BASE_PATH = '/events/';
+const EVENT_PER_PAGE = 16;
+
+// We are using ES modules here in order to be able to import variables from this file in gatsby-node.js
+module.exports = {
+  EVENTS_BASE_PATH,
+  EVENT_PER_PAGE,
+};
