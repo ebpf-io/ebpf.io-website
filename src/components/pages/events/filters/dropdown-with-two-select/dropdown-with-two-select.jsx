dab7b2b4bdaf599caf4be3925b6cc66b8b5917fd
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-03-24 11:49:26 +0300

    feat: adding conference filtering

--- src/components/pages/events/filters/dropdown-with-two-select/dropdown-with-two-select.jsx
+++ src/components/pages/events/filters/dropdown-with-two-select/dropdown-with-two-select.jsx
@@ -1,20 +1,40 @@
+/* eslint-disable react/prop-types */
 import clsx from 'clsx';
-import PropTypes from 'prop-types';
 import React, { useRef, useState } from 'react';
 
 import Checkbox from 'components/shared/checkbox';
 import useClickOutside from 'hooks/use-click-outside';
 import ChevronIcon from 'icons/chevron.inline.svg';
 
-const DropdownSelect = ({ name, items, values, onSelect, className }) => {
+const DropdownWithTwoSelect = ({
+  mainFilter,
+  secondFilter,
+  valuesMain,
+  valuesSecond,
+  onSelectMain,
+  onSelectSecond,
+  className,
+}) => {
   const dropdownRef = useRef(null);
   const buttonRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
-  const handleSelect = (state, value) => {
+
+  const { name, items: itemsMain } = mainFilter;
+  const { items: itemsSecond } = secondFilter;
+
+  const handleSelectMainFilter = (state, value) => {
     if (state) {
-      onSelect([...values, value]);
+      onSelectMain([...valuesMain, value]);
     } else {
-      onSelect(values.filter((currentValue) => currentValue !== value));
+      onSelectMain(valuesMain.filter((currentValue) => currentValue !== value));
+    }
+  };
+
+  const handleSelectSecondFilter = (state, value) => {
+    if (state) {
+      onSelectSecond([...valuesSecond, value]);
+    } else {
+      onSelectSecond(valuesSecond.filter((currentValue) => currentValue !== value));
     }
   };
 
@@ -40,7 +60,7 @@ const DropdownSelect = ({ name, items, values, onSelect, className }) => {
           className={clsx(
             'text-base font-medium leading-none text-gray-40 transition-colors duration-200 group-hover:text-black',
             isOpen && '!text-black',
-            !!values.length && '!font-medium !tracking-normal !text-black'
+            !!valuesMain.length && '!font-medium !tracking-normal !text-black'
           )}
         >
           {name}
@@ -49,7 +69,7 @@ const DropdownSelect = ({ name, items, values, onSelect, className }) => {
           className={clsx(
             'mt-1 h-auto w-2.5 shrink-0 text-gray-40 transition-[transform,color] duration-200 group-hover:text-black',
             isOpen && '-rotate-180',
-            (isOpen || !!values.length) && '!text-black'
+            (isOpen || !!valuesMain.length) && '!text-black'
           )}
         />
       </button>
@@ -60,22 +80,31 @@ const DropdownSelect = ({ name, items, values, onSelect, className }) => {
         )}
         ref={dropdownRef}
       >
-        <nav className="flex items-start justify-between px-4 py-5 md:p-3">
+        <nav className="flex flex-col items-start justify-between px-4 py-5 md:p-3">
           <ul className="flex flex-col">
-            {items.map(({ name }, index) => (
-              <li
-                className={clsx('flex items-center space-x-2.5 pt-2.5 first:pt-0 md:pt-3', {
-                  'pl-7': items.length === 7 && index > 2,
-                })}
-                key={index}
-              >
+            {itemsMain.map(({ name }, index) => (
+              <li className="flex items-center space-x-2.5 pt-2.5 first:pt-0 md:pt-3" key={index}>
                 <Checkbox
                   id={name}
                   name="type"
                   label={name}
                   value={name}
-                  checked={values.includes(name)}
-                  onChange={(e) => handleSelect(e.target.checked, e.target.value)}
+                  checked={valuesMain.includes(name)}
+                  onChange={(e) => handleSelectMainFilter(e.target.checked, e.target.value)}
+                />
+              </li>
+            ))}
+          </ul>
+          <ul className="flex flex-col pl-7 pt-2.5 md:pt-3">
+            {itemsSecond.map(({ name }, index) => (
+              <li className="flex items-center space-x-2.5 pt-2.5 first:pt-0 md:pt-3" key={index}>
+                <Checkbox
+                  id={name}
+                  name="type"
+                  label={name}
+                  value={name}
+                  checked={valuesSecond.includes(name)}
+                  onChange={(e) => handleSelectSecondFilter(e.target.checked, e.target.value)}
                 />
               </li>
             ))}
@@ -86,16 +115,4 @@ const DropdownSelect = ({ name, items, values, onSelect, className }) => {
   );
 };
 
-DropdownSelect.propTypes = {
-  className: PropTypes.string,
-  name: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string.isRequired })).isRequired,
-  values: PropTypes.array.isRequired,
-  onSelect: PropTypes.func.isRequired,
-};
-
-DropdownSelect.defaultProps = {
-  className: null,
-};
-
-export default DropdownSelect;
+export default DropdownWithTwoSelect;
