11a62b6cb813f968545b1d096762f6287c1976da
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-03-24 11:49:26 +0300

    feat: adding conference filtering

--- src/hooks/use-filtered-events.js
+++ src/hooks/use-filtered-events.js
@@ -2,9 +2,10 @@ import { useMemo } from 'react';
 
 const filterEventsData = (eventsData, activeFilters) => {
   const filteredData = eventsData.filter(
-    ({ type, region }) =>
+    ({ type, region, conference }) =>
       (activeFilters.eventtype.length === 0 || activeFilters.eventtype.includes(type)) &&
-      (activeFilters.region.length === 0 || activeFilters.region.includes(region))
+      (activeFilters.region.length === 0 || activeFilters.region.includes(region)) &&
+      (activeFilters.conference.length === 0 || activeFilters.conference.includes(conference))
   );
   return filteredData;
 };
