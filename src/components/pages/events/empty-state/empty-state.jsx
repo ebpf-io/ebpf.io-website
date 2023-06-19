5fed47406396c4d491d44f6d0012f5dd8a197c15
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-06-19 19:09:14 +0300

    feat: minor changes

--- src/components/pages/events/empty-state/empty-state.jsx
+++ src/components/pages/events/empty-state/empty-state.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const EmptyState = () => (
   <section className="container prose-static mt-20 mb-14 max-w-[600px]">
-    <h4 className="text-center">No events were found. Please change the filtering options</h4>
+    <h4 className="text-center">
+      No events match your filters.
+      <br />
+      Try adjusting them for more results.
+    </h4>
   </section>
 );
 
