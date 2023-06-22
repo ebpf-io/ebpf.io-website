ee2bdb1148ed71ab7b666deaece0a31905d32050
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-06-22 18:20:50 +0300

    feat: graphql category logic

--- gatsby/create-labs-page.js
+++ gatsby/create-labs-page.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-const { LABS_BASE_PATH } = require('../src/constants/labs');
+const { LABS_BASE_PATH, LABS_PER_PAGE } = require('../src/constants/labs');
+const slugifyCategory = require('../src/utils/slugify-category');
 
 const { DRAFT_FILTER, LABS_REGEX } = require('./constants');
 
@@ -17,36 +18,15 @@ module.exports = async ({ graphql, actions }) => {
           }
         ) {
           totalCount
-        }
-        allCategories: allMdx(
-          filter: {
-            internal: { contentFilePath: { regex: $labsRegex } }
-            fields: { isDraft: { in: $draftFilter } }
-          }
-        ) {
           group(field: { frontmatter: { category: SELECT } }) {
             fieldValue
           }
-        }
-        allPosts: allMdx(
-          filter: {
-            internal: { contentFilePath: { regex: $labsRegex } }
-            fields: { isDraft: { in: $draftFilter } }
-          }
-          sort: { frontmatter: { date: DESC } }
-        ) {
           nodes {
             frontmatter {
               category
-              title
-              description
-              buttonText
-              buttonUrl
-              ogImage {
-                childImageSharp {
-                  gatsbyImageData(width: 550)
-                }
-              }
+            }
+            internal {
+              contentFilePath
             }
           }
         }
@@ -57,21 +37,70 @@ module.exports = async ({ graphql, actions }) => {
 
   if (result.errors) throw new Error(result.errors);
 
-  const { totalCount } = result.data.allMdx;
-  const { allCategories } = result.data;
-  const {
-    allPosts: { nodes: allPosts },
-  } = result.data;
+  const { group: groupedCategories, totalCount, nodes: allLabs } = result.data.allMdx;
 
-  const postLabs = allPosts.map((labs) => ({ ...labs.frontmatter }));
+  const allLabsCategory = { name: 'All labs', slug: '' };
+  const categories = [
+    allLabsCategory,
+    ...groupedCategories.map(({ fieldValue }) => ({
+      name: fieldValue,
+      slug: slugifyCategory(fieldValue),
+    })),
+  ];
 
-  createPage({
-    path: LABS_BASE_PATH,
-    component: path.resolve('./src/templates/labs.jsx'),
-    context: {
-      postLabs,
-      allCategories,
-      totalCount,
-    },
+  const pageCount = Math.ceil(totalCount / LABS_PER_PAGE);
+
+  const context = {
+    labsCategories: categories,
+    draftFilter: DRAFT_FILTER,
+    limit: LABS_PER_PAGE,
+    labsRegex: LABS_REGEX,
+  };
+
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    const pagePath = i === 0 ? LABS_BASE_PATH : `${LABS_BASE_PATH}${i + 1}`;
+
+    createPage({
+      path: pagePath,
+      component: path.resolve('./src/templates/labs.jsx'),
+      context: {
+        currentPageIndex: i,
+        pageCount,
+        skip: i * LABS_PER_PAGE,
+        ...context,
+      },
+    });
+  });
+
+  categories.forEach(({ name, slug }) => {
+    const categoryPosts = allLabs.filter(
+      ({ frontmatter: { category }, internal: { contentFilePath } }) => {
+        // Required category validation
+        if (!category && !category?.length) {
+          throw new Error(`Post "${contentFilePath}" does not have field "category"!`);
+        }
+
+        return category.includes(name);
+      }
+    );
+
+    const categoryPageCount = Math.ceil(categoryPosts.length / LABS_PER_PAGE);
+
+    Array.from({ length: categoryPageCount }).forEach((_, i) => {
+      const pagePath = i === 0 ? `${LABS_BASE_PATH}${slug}` : `${LABS_BASE_PATH}${slug}/${i + 1}`;
+
+      createPage({
+        path: pagePath,
+        component: path.resolve('./src/templates/labs.jsx'),
+        context: {
+          category: name,
+          categorySlug: slug,
+          currentPageIndex: i,
+          pageCount: categoryPageCount,
+          skip: i * LABS_PER_PAGE,
+          ...context,
+        },
+      });
+    });
   });
 };
--- src/templates/labs.jsx
+++ src/templates/labs.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
+import { graphql } from 'gatsby';
 import React from 'react';
 
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 import SubscriptionForm from 'components/shared/subscription-form';
 
-const EventsPage = ({ pageContext: { postLabs, totalCount, allCategories } }) => {
-  console.log('postLabs', postLabs);
-  console.log('totalCount', totalCount);
-  console.log('allCategories', allCategories);
+const EventsPage = ({
+  pageContext: { labsCategories, pageCount, currentPageIndex, categorySlug },
+  data: {
+    allMdx: { nodes: allLabs },
+  },
+}) => {
+  console.log('labsCategories', labsCategories);
+  console.log('postLabs', allLabs);
+  console.log('totalCount', pageCount);
+  console.log('allCategories', currentPageIndex);
+  console.log('allCategories', categorySlug);
   return (
     <Layout>
       <div className="container flex flex-col pt-20 lg:pt-16">
@@ -24,3 +32,41 @@ const EventsPage = ({ pageContext: { postLabs, totalCount, allCategories } }) =>
 export default EventsPage;
 
 export const Head = ({ location: { pathname } }) => <SEO pathname={pathname} />;
+
+export const query = graphql`
+  query LabsPageQuery(
+    $limit: Int!
+    $skip: Int!
+    $draftFilter: [Boolean]!
+    $labsRegex: String!
+    $category: [String]
+  ) {
+    allMdx(
+      filter: {
+        internal: { contentFilePath: { regex: $labsRegex } }
+        frontmatter: { category: { in: $category } }
+        fields: { isDraft: { in: $draftFilter } }
+      }
+      limit: $limit
+      skip: $skip
+      sort: { frontmatter: { date: DESC } }
+    ) {
+      nodes {
+        frontmatter {
+          category
+          title
+          description
+          date(formatString: "DD MMM, YYYY")
+          buttonText
+          buttonUrl
+          ogImage {
+            childImageSharp {
+              gatsbyImageData(width: 490)
+            }
+            publicURL
+          }
+        }
+      }
+    }
+  }
+`;
