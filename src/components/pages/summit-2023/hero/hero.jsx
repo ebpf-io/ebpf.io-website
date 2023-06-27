8249684b837d26885af3be0b1b902efd941c3d02
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-06-27 13:58:33 +0300

    feat: adds time

--- src/components/pages/summit-2023/hero/hero.jsx
+++ src/components/pages/summit-2023/hero/hero.jsx
@@ -13,7 +13,7 @@ const icons = {
   slack: SlackIcon,
 };
 
-const Hero = ({ date, title, description }) => {
+const Hero = ({ date, title, time, description }) => {
   const [isOpen, setIsOpen] = useState(false);
   const openModal = () => {
     setIsOpen(true);
@@ -42,22 +42,24 @@ const Hero = ({ date, title, description }) => {
 
   return (
     <section className="relative lg:overflow-hidden">
-      <div className="absolute right-0 -top-20 w-[200px] lg:block md:-right-4 sm:-top-[120px] sm:hidden">
+      <div className="absolute -top-20 right-0 w-[200px] lg:block md:-right-4 sm:-top-[120px] sm:hidden">
         <StaticImage src="./images/drop-1.jpg" alt="" loading="eager" aria-hidden />
       </div>
       <div className="absolute -top-3 left-0 w-[128px] 2xl:-left-4 2xl:top-12 xl:-left-8 lg:-top-3 md:-left-4 sm:-left-14 sm:-top-[28px] sm:hidden">
         <StaticImage src="./images/drop-2.jpg" alt="" loading="eager" aria-hidden />
       </div>
-      <div className="absolute top-96 -left-10 w-[330px] 2xl:top-[450px] 2xl:-left-20 2xl:w-[280px] lg:top-80 lg:left-0 lg:block md:top-64 md:-left-20 sm:hidden">
+      <div className="absolute -left-10 top-96 w-[330px] 2xl:-left-20 2xl:top-[450px] 2xl:w-[280px] lg:left-0 lg:top-80 lg:block md:-left-20 md:top-64 sm:hidden">
         <StaticImage src="./images/honey.jpg" alt="" loading="eager" aria-hidden />
       </div>
-      <div className="container relative pt-28 pb-6 lg:w-[95%] lg:pt-16 lg:pb-0 md:pt-8 ">
-        <div className="max-w-[644px] lg:max-w-full lg:text-center">
-          <time
-            className="with-orange-highlight inline-block rounded-md border-2 border-primary-orange border-opacity-30 bg-white p-2 text-center font-bold uppercase leading-none text-black"
-            dangerouslySetInnerHTML={{ __html: date }}
-          />
-
+      <div className="container relative pb-6 pt-28 lg:w-[95%] lg:pb-0 lg:pt-16 md:pt-8 ">
+        <div className="flex max-w-[644px] flex-col lg:max-w-full lg:text-center">
+          <div className="flex items-center gap-x-5 lg:mx-auto sm:flex-col sm:gap-x-0 sm:gap-y-4">
+            <time
+              className="with-orange-highlight inline-block rounded-md border-2 border-primary-orange border-opacity-30 bg-white p-2 text-center font-bold uppercase leading-none text-black"
+              dangerouslySetInnerHTML={{ __html: date }}
+            />
+            <span className="text-sm uppercase" dangerouslySetInnerHTML={{ __html: time }} />
+          </div>
           <h1 className="mt-7 text-11xl font-bold leading-tight sm:text-7xl">{title}</h1>
           <div
             className="with-link-primary mt-4 max-w-lg space-y-4 font-sans text-xl font-normal lg:mx-auto lg:max-w-xl md:max-w-lg md:space-y-4 md:text-lg sm:max-w-sm"
@@ -106,6 +108,7 @@ Hero.propTypes = {
   date: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
 };
 
 export default Hero;
