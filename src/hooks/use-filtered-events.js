7fb7edb4681fc606f5f8f4dc27b8af5c11acc9a0
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-07-21 11:34:32 +0300

    feat: adds new filter

--- src/hooks/use-filtered-events.js
+++ src/hooks/use-filtered-events.js
@@ -1,11 +1,26 @@
 import { useMemo } from 'react';
 
+import { eventFilters } from 'constants/event-filters';
+
+const upcomingDate = eventFilters[3].items[0].name;
+const archivedDate = eventFilters[3].items[1].name;
+
 const filterEventsData = (eventsData, activeFilters) => {
-  const filteredData = eventsData.filter(({ type, region, conference }) => {
+  const filteredData = eventsData.filter(({ type, region, conference, date }) => {
+    const eventDate = new Date(date);
+    const now = new Date();
+
+    const isDateSelected =
+      activeFilters.date.length === 0 ||
+      (activeFilters.date.includes(upcomingDate) && eventDate > now) ||
+      (activeFilters.date.includes(archivedDate) && eventDate < now);
+
     const isEventTypeSelected =
       activeFilters.eventtype.length === 0 || activeFilters.eventtype.includes(type);
+
     const isConferenceSelected =
       activeFilters.conference.length === 0 || activeFilters.conference.includes(conference);
+
     const isRegionSelected =
       activeFilters.region.length === 0 || activeFilters.region.includes(region);
 
@@ -13,7 +28,7 @@ const filterEventsData = (eventsData, activeFilters) => {
       return (isEventTypeSelected || isConferenceSelected) && isRegionSelected;
     }
 
-    return isEventTypeSelected && isConferenceSelected && isRegionSelected;
+    return isEventTypeSelected && isConferenceSelected && isRegionSelected && isDateSelected;
   });
 
   return filteredData;
