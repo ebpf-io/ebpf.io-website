36a80c403f4ac6068524198dff2db4638605f271
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-08-15 20:40:44 +0300

    fix: minor changes

--- src/components/pages/summit-schedule/grid/grid.jsx
+++ src/components/pages/summit-schedule/grid/grid.jsx
@@ -4,10 +4,8 @@ import DangerousHtml from 'components/shared/dangerous-html';
 
 const Grid = () => {
   const [scheduleHTML, setScheduleHTML] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
 
   const fetchScheduleData = async (cb) => {
-    setIsLoading(true);
     try {
       const res = await fetch(process.env.GATSBY_SESSIONIZE_URL);
       const data = await res.text();
@@ -15,8 +13,6 @@ const Grid = () => {
     } catch (e) {
       // eslint-disable-next-line
       console.warn('Can not fetch the schedule:', { e });
-    } finally {
-      setIsLoading(false);
     }
   };
 
@@ -35,28 +31,22 @@ const Grid = () => {
     }
   }, [modifySessionizeScript]);
 
-  if (isLoading) {
-    return (
-      <div className="my-20 flex items-center justify-center">
-        <div className="h-10 w-10 animate-spin rounded-full border-b-2 border-t-2 border-gray-80" />
+  return (
+    <section className="safe-paddings py-24 md:py-20">
+      <div className="container item-center flex flex-col" id="schedule">
+        <h2 className="mx-auto w-fit text-10xl font-bold leading-tight lg:text-9xl sm:text-7xl">
+          Schedule
+        </h2>
+        {scheduleHTML ? (
+          <DangerousHtml className="mt-6" html={scheduleHTML} />
+        ) : (
+          <div className="my-20 flex items-center justify-center">
+            <div className="h-10 w-10 animate-spin rounded-full border-b-2 border-t-2 border-gray-80" />
+          </div>
+        )}
       </div>
-    );
-  }
-
-  if (!isLoading) {
-    return (
-      <section className="safe-paddings py-24 md:py-20">
-        <div className="container item-center flex flex-col" id="schedule">
-          <h2 className="mx-auto w-fit text-10xl font-bold leading-tight lg:text-9xl sm:text-7xl">
-            Schedule
-          </h2>
-          {scheduleHTML ? <DangerousHtml className="mt-6" html={scheduleHTML} /> : null}
-        </div>
-      </section>
-    );
-  }
-
-  return null;
+    </section>
+  );
 };
 
 export default Grid;
