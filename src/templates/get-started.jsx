79f6ca90ad05cee39e44425ef1a4722f52ca0638
prosazhin <prosazhin@gmail.com>
2023-09-12 17:51:48 +0200

    fix: fixes after translate

--- src/templates/get-started.jsx
+++ src/templates/get-started.jsx
@@ -13,26 +13,27 @@ import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 import SubscriptionForm from 'components/shared/subscription-form';
 import VideoGallery from 'components/shared/video-gallery';
-import videoItems from 'data/get-started/video-data';
+import data from 'data/pages/get-started';
+import SEO_DATA from 'data/shared/seo-data';
+
+import { defaultLanguage } from '../../config/languages';
+
+const lang = defaultLanguage;
 
 const GetStartedPage = ({ pageContext: { events, blogPosts, contributors } }) => {
   const hasContributors = false;
 
   return (
-    <Layout>
-      <Hero />
-      <Documentation />
+    <Layout pageUrls={data.pageUrls}>
+      <Hero {...data[lang].hero} />
+      <Documentation {...data[lang].documentation} />
       <div className="container mt-28 md:mt-20 sm:mt-12">
-        <SubscriptionForm size="lg" />
+        <SubscriptionForm size="lg" {...data[lang].subscriptionForm} />
       </div>
-      <Books />
-      <Tutorials />
+      <Books {...data[lang].books} />
+      <Tutorials {...data[lang].tutorials} />
       <Events items={events.map(({ frontmatter }) => ({ ...frontmatter }))} />
-      <VideoGallery
-        className="mt-32 lg:mt-24 md:mt-16 sm:mt-14"
-        items={videoItems}
-        title="eBPF introductory videos"
-      />
+      <VideoGallery className="mt-32 lg:mt-24 md:mt-16 sm:mt-14" {...data[lang].videoGallery} />
       <BlogPosts
         className={clsx(!hasContributors && 'mb-36 xl:mb-32 lg:mb-28 md:mb-20')}
         items={blogPosts.map(({ frontmatter }) => ({
@@ -53,4 +54,6 @@ const GetStartedPage = ({ pageContext: { events, blogPosts, contributors } }) =>
 
 export default GetStartedPage;
 
-export const Head = ({ location: { pathname } }) => <SEO pathname={pathname} />;
+export const Head = ({ location: { pathname } }) => (
+  <SEO pathname={pathname} {...SEO_DATA.home[lang]} />
+);
