818feb5a7c71900bd56f28c2f3bcc17fee8a5c61
hacktivist123 <akintayoshedrack@gmail.com>
2023-09-15 09:28:39 +0100

    chore: add ebpf user group

--- src/data/shared/header-menu.js
+++ src/data/shared/header-menu.js
@@ -149,6 +149,13 @@ const headerMenu = [
       },
       {
         title: {
+          en: 'eBPF User Group',
+          'fr-fr': 'eBPF User Group',
+        },
+        to: 'https://www.meetup.com/ebpf-user-group/',
+      },
+      {
+        title: {
           en: 'Stack Overflow',
           'fr-fr': 'Stack Overflow',
         },
