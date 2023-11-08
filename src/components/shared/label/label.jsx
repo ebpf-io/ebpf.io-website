8ac3a530d3a9a0b870bb4ecc76485f1982aa8552
americano98 <mr.nik.bazarov@gmail.com>
2023-11-08 13:08:13 +0300

    refactor: label component

--- src/components/shared/label/label.jsx
+++ src/components/shared/label/label.jsx
@@ -2,27 +2,29 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Label = ({ type, className }) => (
-  <span
+const Label = ({ theme, children, className }) => (
+  <mark
     className={clsx(
       'w-fit rounded pb-[4.5px] pl-1.5 pr-[5px] pt-[3.5px] text-xs font-bold uppercase leading-none tracking-middle-wide',
-      { 'bg-secondary-green-light text-[#008015]': type === 'Meetup' },
-      { 'bg-secondary-red-light text-[#CC3333]': type === 'Webinar' },
-      { 'bg-secondary-blue-light text-[#00779E]': type === 'Conference' },
+      { 'bg-secondary-green-light text-[#008015]': theme === 'green' },
+      { 'bg-secondary-red-light text-[#CC3333]': theme === 'red' },
+      { 'bg-secondary-blue-light text-[#00779E]': theme === 'blue' },
       className
     )}
   >
-    {type}
-  </span>
+    {children}
+  </mark>
 );
 
 Label.propTypes = {
-  type: PropTypes.string.isRequired,
   className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  theme: PropTypes.oneOf(['green', 'red', 'blue']),
 };
 
 Label.defaultProps = {
   className: null,
+  theme: 'blue',
 };
 
 export default Label;
