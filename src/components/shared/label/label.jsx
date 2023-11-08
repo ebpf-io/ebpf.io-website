70b103cd489b434fdc9e4ddf2c8d2efab1f20234
americano98 <mr.nik.bazarov@gmail.com>
2023-11-08 14:19:43 +0300

    chore: sepate themes

--- src/components/shared/label/label.jsx
+++ src/components/shared/label/label.jsx
@@ -2,13 +2,17 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const themes = {
+  green: 'bg-secondary-green-light text-[#008015]',
+  red: 'bg-secondary-red-light text-[#CC3333]',
+  blue: 'bg-secondary-blue-light text-[#00779E]',
+};
+
 const Label = ({ theme, children, className }) => (
   <mark
     className={clsx(
       'inline-block w-fit rounded px-1.5 pb-[4.5px] pt-[3.5px] text-xs font-semibold uppercase leading-none tracking-wide',
-      { 'bg-secondary-green-light text-[#008015]': theme === 'green' },
-      { 'bg-secondary-red-light text-[#CC3333]': theme === 'red' },
-      { 'bg-secondary-blue-light text-[#00779E]': theme === 'blue' },
+      themes[theme],
       className
     )}
   >
