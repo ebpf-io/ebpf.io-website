1b192389c7d4a710ae689a6523a6bf8424a05ac3
Nikita <105175118+nikvoblikov@users.noreply.github.com>
2023-11-13 16:26:12 +0300

    feat: updates cards design

--- src/components/shared/case-studies-list/case-studies-list.jsx
+++ src/components/shared/case-studies-list/case-studies-list.jsx
@@ -6,15 +6,10 @@ import Link from 'components/shared/link';
 
 const CaseStudiesList = ({ className, items, linkUrl, linkText }) => (
   <>
-    <ul className={clsx('grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1', className)}>
+    <ul className={clsx('grid-gap grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1', className)}>
       {items.map(({ title, logo: Logo, description, links }, index) => (
         <li
-          className={clsx(
-            'flex flex-col items-start border-l border-t border-dashed border-gray-80 p-10 lg:p-8',
-            '[&:nth-child(-n+3)]:border-t-0 [&:nth-child(3n+1)]:border-l-0',
-            'md:[&:nth-child(-n+2)]:border-t-0 md:[&:nth-child(-n+3)]:border-t md:[&:nth-child(2n+1)]:border-l-0 md:[&:nth-child(3n+1)]:border-l',
-            'sm:!border-t sm:border-l-0 sm:px-0 sm:first:!border-t-0 sm:first:pt-0 sm:last:pb-0 sm:[&:nth-child(3n+1)]:border-l-0'
-          )}
+          className="flex h-full flex-col self-stretch rounded-lg border border-gray-90 bg-white p-8 duration-200 hover:drop-shadow-book sm:p-6"
           key={index}
         >
           <div className="max-w-full">
@@ -23,10 +18,10 @@ const CaseStudiesList = ({ className, items, linkUrl, linkText }) => (
           </div>
 
           <p
-            className="mb-7 mt-6 text-lg lg:mb-5 lg:text-base [&_strong]:font-semibold"
+            className="mb-6 mt-6 text-xl leading-normal lg:mb-5 lg:text-base [&_strong]:font-semibold"
             dangerouslySetInnerHTML={{ __html: description }}
           />
-          <ul className="mt-auto flex flex-wrap gap-x-4 gap-y-4">
+          <ul className="mt-auto flex flex-wrap gap-x-4 gap-y-4 border-t border-dashed border-gray-90 pt-7 sm:pt-6">
             {links.map(({ text, to }, index) => (
               <li
                 className="relative text-sm font-semibold uppercase leading-none tracking-wider before:absolute before:-right-2.5 before:top-1/2 before:block before:h-1 before:w-1 before:-translate-y-1/2 before:rounded-full before:bg-gray-90 last:before:hidden lg:text-[13px]"
