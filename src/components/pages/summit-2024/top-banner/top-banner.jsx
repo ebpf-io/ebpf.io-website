6d8e3bd150f0f0c83ca09da8b7b8e4fa09ca40e4
Bill Mulligan <billmulligan516@gmail.com>
2024-05-30 18:05:26 +0200

    update CfP dates
    Signed-off-by: Bill Mulligan <billmulligan516@gmail.com>

--- src/components/pages/summit-2024/top-banner/top-banner.jsx
+++ src/components/pages/summit-2024/top-banner/top-banner.jsx
@@ -6,7 +6,7 @@ const TopBanner = () => (
   <div className="relative flex h-[48px] flex-row items-center justify-center space-x-6 overflow-hidden bg-black py-2 sm:h-auto sm:flex-col sm:space-x-0 sm:space-y-1 sm:pb-5">
     <div className="flex flex-row items-center">
       <span className="font-sans text-lg font-medium leading-none text-white md:text-base">
-        CFPs opens from 5th to 17th July
+        CFPs open until 17th July
       </span>
     </div>
     <Link theme="orange" size="lg" to="https://sessionize.com/ebpf-summit-2024/" target="_blank">
