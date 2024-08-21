01099bf16228248596d2b3878e44488068299c0d
Paul Arah <paularah.self@gmail.com>
2024-08-21 12:10:05 +0100

    fix: create dedciated sessionize endpoint for the summit

--- src/pages/summit-2024-schedule.jsx
+++ src/pages/summit-2024-schedule.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
+import SummitLayout from 'components/pages/summit-2024/summit-layout';
 import Hero from 'components/pages/summit-schedule/hero';
 import SEO from 'components/shared/seo';
-import SummitLayout from 'components/shared/summit-layout';
 import { navigation, hubspotFormId } from 'data/shared/summit-2023-navigation';
 
 const ogImage = '/images/social-preview-ebpf-summit.jpg';
@@ -20,7 +20,7 @@ const Summit2024 = () => (
       className="px-4 mx-4"
       dangerouslySetInnerHTML={{
         __html: `
-            <script type="text/javascript" src="https://sessionize.com/api/v2/gjpcd45i/view/GridSmart"></script>
+            <script type="text/javascript" src="https://sessionize.com/api/v2/t472wio9/view/GridSmart"></script>
           `,
       }}
     />
@@ -30,7 +30,7 @@ const Summit2024 = () => (
 // eslint-disable-next-line react/prop-types
 export const Head = ({ location: { pathname } }) => {
   const pageMetadata = {
-    title: 'eBPF Summit 2024 Talks',
+    title: 'eBPF Summit 2024 Schedule',
     description:
       'Explore the talks of the eBPF Summit, a virtual event for all things within the Open Source eBPF ecosystem',
     image: ogImage,
