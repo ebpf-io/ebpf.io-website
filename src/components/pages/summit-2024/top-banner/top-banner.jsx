56bd2ebd3c252e3987e2ea05cd10f1d7abddb6f1
Paul Arah <paularah.self@gmail.com>
2024-08-21 22:59:14 +0100

    eBPF summit 2024 schedule (#631)
    * feat: initial schedule test
    
    * fix: create dedciated sessionize endpoint for the summit
    
    * fix: hackish solution sessionize
    
    * fix: cleanup
    
    * fix: add schedule banner
    
    ---------
    
    Signed-off-by: Paul Arah <paularah.self@gmail.com>

--- src/components/pages/summit-2024/top-banner/top-banner.jsx
+++ src/components/pages/summit-2024/top-banner/top-banner.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import Link from 'components/shared/link';
+
 const TopBanner = () => (
   <div className="relative flex h-[48px] flex-row items-center justify-center space-x-6 overflow-hidden bg-black py-2 sm:h-auto sm:flex-col sm:space-x-0 sm:space-y-1 sm:pb-5">
     <div className="flex flex-row items-center">
-      <span className="font-sans text-lg font-medium leading-none text-orange-1 md:text-base">
-        Call for proposal closed
+      <span className="font-sans text-lg font-medium leading-none text-white md:text-base">
+        eBPF Summit 2024 Schedule is now live!
       </span>
     </div>
+    <Link theme="orange" size="lg" to="/summit-2024-schedule" target="_blank">
+      View the schedule
+    </Link>
   </div>
 );
 
