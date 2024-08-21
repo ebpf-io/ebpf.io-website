bb483f6ee2ea81d1857fc282825619e659bb05cd
Paul Arah <paularah.self@gmail.com>
2024-08-21 22:51:50 +0100

    fix: cleanup

--- src/components/pages/summit-2024/top-banner/top-banner.jsx
+++ src/components/pages/summit-2024/top-banner/top-banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Link from 'components/shared/link';
+
 const TopBanner = () => (
   <div className="relative flex h-[48px] flex-row items-center justify-center space-x-6 overflow-hidden bg-black py-2 sm:h-auto sm:flex-col sm:space-x-0 sm:space-y-1 sm:pb-5">
     <div className="flex flex-row items-center">
