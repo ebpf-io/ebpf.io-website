dbda3c5ac3840f4ebf5fe47b97176833a6a9db9d
Murvin Steve Morel <msmorel@users.noreply.github.com>
2024-09-20 11:55:19 +0100

    PR: Restrict Bot Crawler Access (#649)
    * feat: add gitignore rule for local netlify dev testing files
    
    * chore: remove inactive edge functions file
    
    * feat: add new netlify _headers file and _redirect rules to block crawlers with 403
    
    * refactor: add empty line
    
    * feat: disable the mergeSecurityHeaders option in gatsby-plugin-netlify
    
    * chore: remove gatsby-plugin-netlify update
    
    * feat: add updated netlify function to handle bot traffic
    
    * refactor: temporarily remove netlify function
    
    * refactor: tweak _headers file
    
    * fix: fix _headers file comments
    
    * refactor: tweak changes to use updated netlify function
    
    * chore: remove changes added by netlify cli
    
    * feat: perform final tweaks on bot function
    
    * chore: remove netlify update to gitignore file

--- netlify/edge-functions/bots.mjs
+++ netlify/edge-functions/bots.mjs
@@ -3,28 +3,25 @@ export default async (request) => {
   const ua = request.headers.get('user-agent');
 
   // Specify the user agents which need to be blocked
-  const agents = ['Wget', 'Go-http-client', 'Gofeed', '-'];
+  const agents = ['Wget', 'Go-http-client', 'Gofeed', 'Undefined', '-'];
 
   // Check against our list of known AI bots
   let isBot = false;
   agents.forEach((agent) => {
     if (ua?.toLowerCase().includes(agent.toLowerCase())) {
       isBot = true;
-      
     }
   });
 
-  // If the requester is an AI bot, disallow with a 401
+  // If the requester is an AI bot, disallow with a 403
   if (isBot) {
-    return new Response(null, { status: 401 });
+    return new Response(null, { status: 403 });
   }
+
   // Otherwise, continue with the request as normal
-  
-    
-  
 };
 
 // This edge function is executed for all requests across the site
 export const config = {
-  path: '*',
+  path: '/*',
 };
