9e430a8db07bf9eddd8615696d4495d88a91a8a4
Paul Arah <paularah.self@gmail.com>
2024-09-25 14:50:41 +0100

    feat: post-ebpf summit updates (#653)

--- src/components/pages/summit-2024/top-banner/top-banner.jsx
+++ src/components/pages/summit-2024/top-banner/top-banner.jsx
@@ -6,11 +6,16 @@ const TopBanner = () => (
   <div className="relative flex h-[48px] flex-row items-center justify-center space-x-6 overflow-hidden bg-black py-2 sm:h-auto sm:flex-col sm:space-x-0 sm:space-y-1 sm:pb-5">
     <div className="flex flex-row items-center">
       <span className="font-sans text-lg font-medium leading-none text-white md:text-base">
-        eBPF Summit 2024 Schedule is now live!
+        Watch the recordings on Youtube!
       </span>
     </div>
-    <Link theme="orange" size="lg" to="/summit-2024-schedule" target="_blank">
-      View the schedule
+    <Link
+      theme="orange"
+      size="lg"
+      to="https://www.youtube.com/playlist?list=PLDg_GiBbAx-m7yn_FYcc41PNrgtxlISBK"
+      target="_blank"
+    >
+      Watch
     </Link>
   </div>
 );
