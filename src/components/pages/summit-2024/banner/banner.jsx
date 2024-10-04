8c193956a0bf28cfa67aa056d3eafb656c88685a
Paul Arah <paularah.self@gmail.com>
2024-10-04 16:57:33 +0100

    fix: add talk link page and button

--- src/components/pages/summit-2024/banner/banner.jsx
+++ src/components/pages/summit-2024/banner/banner.jsx
@@ -1,43 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Button from 'components/shared/button/button';
-import FormModal from 'components/shared/form-modal';
-import { hubspotFormId } from 'data/shared/summit-2023-navigation';
+import Link from 'components/shared/link/link';
 
-const Banner = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const closeModal = (e) => {
-    e.stopPropagation();
-    setIsOpen(false);
-  };
-
-  return (
-    <section className="relative mt-40 md:mt-16 sm:mt-14">
-      <div className="container">
-        <div className="relative flex flex-row items-center justify-center space-x-10 rounded-lg bg-gray-98 px-6 py-[42px] md:flex-col md:space-x-0 md:space-y-6 md:py-6">
-          <h2 className="heading-6xl text-left font-semibold leading-tight text-black md:text-center">
-            Find all of the recordings on YouTube
-          </h2>
-          <Button
-            to="https://www.youtube.com/playlist?list=PLDg_GiBbAx-m7yn_FYcc41PNrgtxlISBK"
-            className="rounded-lg leading-tight xs:w-full"
-            theme="black-filled"
-            size="md"
-            target="_blank"
-          >
-            Watch now
-          </Button>
-        </div>
+const Banner = () => (
+  <section className="relative mt-40 md:mt-16 sm:mt-14">
+    <div className="container">
+      <div className="relative flex flex-row items-center justify-center space-x-10 rounded-lg bg-gray-98 px-6 py-[42px] md:flex-col md:space-x-0 md:space-y-6 md:py-6">
+        <h2 className="heading-6xl text-left font-semibold leading-tight text-black md:text-center">
+          Find all of the recordings on YouTube
+        </h2>
+        <Link
+          to="summit-2024-talks"
+          className="rounded-lg leading-tight xs:w-full"
+          theme="black-filled"
+          size="md"
+        >
+          View talks
+        </Link>
       </div>
-      <FormModal
-        title="Register for eBPF Summit"
-        isOpen={isOpen}
-        closeModal={closeModal}
-        hubspotFormId={hubspotFormId}
-      />
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default Banner;
