71638f2c17c4be27eada3d0f4f3a4d42929ea39f
David Coles <David.Coles@global.com>
2024-12-21 19:02:36 +0000

    Fixed 'laod' typo (#676)

--- src/data/pages/applications/emerging.js
+++ src/data/pages/applications/emerging.js
@@ -484,7 +484,7 @@ const emergingApplications = [
     name: 'vc5',
     logoName: 'vc5Logo',
     title: 'Layer 4 load balancer for Linux',
-    description: `vc5 is an XDP based Direct Server Return (DSR) layer 4 laod balancer with a use case in replacing
+    description: `vc5 is an XDP based Direct Server Return (DSR) layer 4 load balancer with a use case in replacing
     hardware appliances. Service health checks and BGP-based Route Health Injection (RHI) are built in. A web console,
     JSON stats endpoint, Prometheus metrics, logging with Elasticsearch, and alerting with webhooks are included.`,
     urls: [
