045036981ef257f887716570ed06748f4d5044de
Paul Arah <paularah.self@gmail.com>
2025-06-11 11:24:37 +0100

    fix: 4 col grid for large screens

--- src/components/pages/project-landscape/projects-list/projects-list.jsx
+++ src/components/pages/project-landscape/projects-list/projects-list.jsx
@@ -103,7 +103,7 @@ const ProjectsList = ({ className, titleTag: Tag, title, description, items }) =
       >
         {isLargeScreen && viewMode === 'grid' ? (
           <div className="container mt-12 md:mt-10">
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="grid grid-cols-4 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {items.map((item, index) => (
                 <ProjectCard key={index} {...item} viewMode="grid" />
               ))}
